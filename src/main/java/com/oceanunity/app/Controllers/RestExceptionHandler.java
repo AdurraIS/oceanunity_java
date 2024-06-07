c732f2632f44349bbd2310934dcd6ae8e46f09e8
Giovanni Ultramari <giovanniultramari@gmail.com>
2024-06-07 04:24:28 -0300

    FEAT: Adding handleMethodArgumentNotValidExcpetion

--- src/main/java/com/oceanunity/app/Controllers/RestExceptionHandler.java
+++ src/main/java/com/oceanunity/app/Controllers/RestExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.oceanunity.app.Controllers;
 
 import com.oceanunity.app.Exceptions.OceanUnityException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,16 @@ public class RestExceptionHandler {
     public ProblemDetail handleOceanUnityException(OceanUnityException exception){
         return exception.toProblemDetail();
     }
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidExcpetion(MethodArgumentNotValidException e){
+        var fieldErrors = e.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pb.setTitle("Your request parameters didn't validate");
+        pb.setProperty("Invalid Params", fieldErrors);
+        return pb;
+    }
+    private record InvalidParam(String fieldName, String reason){};
 }
